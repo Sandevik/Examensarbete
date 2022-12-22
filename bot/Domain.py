f10bd505ef5445afbbeb1d298d1df8cf052574ad
[Simon] <[simon.sandevik@outlook.com]>
2022-12-22 17:17:33 +0100

    bot klar

--- bot/Domain.py
+++ bot/Domain.py
@@ -9,6 +9,13 @@ class Domain:
         self.domainUrl = idna.decode(encodedDomainUrl)
         self.domainNameRating = domainNameRating
         self.dR = dR
+        self.pageAuth = None
+        self.pagesCrawledFromRoot = None
+        self.pageTitle = None
+        self.lastCrawled = None
+        self.liklyFree = None
+        self.spamScore = None
+        self.pagesToPage = None
         self.externalLinks = externalLinks
         if self.domainNameRating is None and not re.compile(REGEXPATTERN, re.IGNORECASE).match(self.domainUrl):
            self.domainNameRating = "bad"
@@ -19,7 +26,49 @@ class Domain:
 
     def getDomain(self):
         return self.domainUrl
+    def getEncoded(self):
+        return self.encodedDomainUrl
+    def getAvailableBy(self):
+        return self.availableBy
+    def getDomainNameRating(self):
+        return self.domainNameRating
+    def getDR(self):
+        return self.dR
+    def getExternalLinks(self):
+        return self.externalLinks
+    def setLastCrawled(self, value):
+        self.lastCrawled = value
+    def setPageTitle(self, value):
+        self.pageTitle = value
+        self.liklyFree = "park" in value.lower()
+    def setPagesToPage(self, value):
+        self.pagesToPage = value
+    def setDomainAuth(self, value):
+        self.dR = value
+    def setSpamScore(self, value):
+        self.spamScore = value
+    def setPageAuth(self, value):
+        self.pageAuth = value
+    def setExternalLinks(self, value):
+        self.externalLinks = value
+    def setPagesCrawledFromRoot(self, value):
+        self.pagesCrawledFromRoot = value
     
-    def ToString(self):
-        return "url: "+ self.domainUrl + ", encodedUrl: " + self.encodedDomainUrl + ", domainNameRating: " + self.domainNameRating + ", avaliableBy: " + self.availableBy
-           
+
+    def toDict(self):
+        ref = {
+            u"encodedDomainUrl": self.encodedDomainUrl, 
+            u"domainUrl": self.domainUrl, 
+            u"availableBy": self.availableBy, 
+            u"domainNameRating": self.domainNameRating, 
+            u"domainAuthority" : self.dR, 
+            u"externalLinks" : self.externalLinks,
+            u"pageTitle" : self.pageTitle, 
+            u"lastCrawled" : self.lastCrawled, 
+            u"pagesToPage": self.pagesToPage, 
+            u"spamScore": self.spamScore, 
+            u"pageAuthority" : self.pageAuth,
+            u"likelyFree" : self.liklyFree,
+            u"pagesCrawledFromRoot" : self.pagesCrawledFromRoot
+        }
+        return ref
--- bot/bot.py
+++ bot/bot.py
@@ -1,12 +1,26 @@
-import requests
-import json
-import re
-import idna
+import json, requests, os, asyncio, datetime
+import firebase_admin
+from firebase_admin import firestore
+from firebase_admin import credentials
 from Domain import Domain
+
 SE_DOMAINS = "https://data.internetstiftelsen.se/bardate_domains.json"
 NU_DOMAINS = "https://data.internetstiftelsen.se/bardate_domains_nu.json"
-REGEXPATTERN = "(\d{0,4}-?[A-Za-zåäöÅÄÖ]{3,}\.(se|nu))|(\d{0,4}-?[A-Za-zåäöÅÄÖ]{3,}\d{0,3}\.(se|nu))"
-
+BASE_MOZ_URL = "https://lsapi.seomoz.com/v2/"
+MOZ_ACCESS_ID = "mozscape-c64bc021ef"
+MOZ_SECRET  = "da12db1db142707d76e2cdb657e5089b"
+REGEXPATTERN = "(\d{0,2}-?[A-Za-zåäöÅÄÖ]{3,}\.(se|nu))|(\d{0,4}-?[A-Za-zåäöÅÄÖ]{3,}\d{0,3}\.(se|nu))"
+file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)),"examensarbete-a4732-firebase-adminsdk-24swr-b794114203.json")
+cred = credentials.Certificate(file_path)
+app = firebase_admin.initialize_app(cred)
+db = firestore.client()
+def checkIfInFireBase(domain):
+    #Kolla om domänen med domännamnet redan finns i firebase
+    doc_ref = db.collection(u"domains").where(u"domainUrl", u"==", domain.getDomain()).get()
+    return len(doc_ref) == 1
+def sendToFireBase(domain):
+    #skicka värden till firebase
+    db.collection(u"domains").document(domain.getDomain()).set(domain.toDict())
 def getInitalDomains():
     # Hämta Domäner och skapa en dictionary av dem, returna array med dictionary.
     resSE = requests.get(SE_DOMAINS)
@@ -14,28 +28,56 @@ def getInitalDomains():
     resNU = requests.get(NU_DOMAINS)
     resNU = json.loads(resNU.text)
     res = resSE["data"] + resNU["data"]
-    
+
     # Skapa instanser av domänklassen och lägga till den i en lista
     for i in range(len(res)):
         domain = Domain(res[i]["name"], res[i]["release_at"])
-        domains.append(domain)
-    
+        domains.append(domain)        
 
-
-def checkIfInFireBase(obj):
-    #Kolla om obj med domännamnet redan finns i firebase
-    pass
-def fetchMozValues():
+def fetchMozValues(domain):
     #Skicka till moz och ta reda på domain ratings, external links m.m (De som är ok i domainNameRating)
-    pass
-def sendToFireBase():
-    #skicka värden till firebase
-    pass
+    domainUrl = domain.getDomain()
+    auth = (MOZ_ACCESS_ID, MOZ_SECRET)
+    url = "https://lsapi.seomoz.com/v2/url_metrics"
+    data = '{"targets": ["'+domainUrl+'"]}'
+    try:
+        res = requests.post(url, data=data, auth=auth)
+        data = json.loads(res.text)["results"]
 
+        domain.setPageTitle(data[0]["title"])
+        domain.setPagesToPage(data[0]["pages_to_page"])
+        domain.setDomainAuth(data[0]["domain_authority"])
+        domain.setPageAuth(data[0]["page_authority"])
+        domain.setSpamScore(data[0]["spam_score"])
+        domain.setLastCrawled(data[0]["last_crawled"])
+        domain.setExternalLinks(data[0]["external_pages_to_root_domain"])
+        domain.setPagesCrawledFromRoot(data[0]["pages_crawled_from_root_domain"])
+    except:
+        pass
 
+def run():
+
+    maxDate = datetime.datetime(2023, 1, 23)
+
+    mozReq = 0
+    for i in range(len(domains)):
+        j = i+2401 # + 2400 då jag vill ha värden som är mer i mitten
+        dateArray = domains[j].getAvailableBy().split("-")
+        date = datetime.datetime(int(dateArray[0]) , int(dateArray[1]), int(dateArray[2]))
+        if domains[j].getDomainNameRating() == "ok" and date < maxDate and mozReq < 2500:
+            mozReq += 1
+            print(f"mozReq: {mozReq}, i: {i}")
+            fetchMozValues(domains[j])
+            sendToFireBase(domains[j])
+        #elif mozReq < 2500: # Om jag vill skicka alla som inte får värden till firebase.
+            # sendToFireBase(domains[j])  
+        else:
+            break
+
+    
 domains = []
 getInitalDomains()
-print(domains[2].ToString())
+run()
 
 
 
